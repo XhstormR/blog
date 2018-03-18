b1312208d2ecb643bd2caa007fa9e8ceb1ec94ff
XhstormR <10527522+XhstormR@users.noreply.github.com>
2018-03-18 23:54:22 +0800

    Signed-off-by: XhstormR <10527522+XhstormR@users.noreply.github.com>

--- scripts/hugo.sh
+++ scripts/hugo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-jq --rawfile abc graphql.txt '.query=$abc' template.json | \
+./jq --rawfile abc graphql.txt '.query=$abc' template.json | \
 curl -sk -H "Authorization: bearer $GITHUB_TOKEN" -d @- https://api.github.com/graphql | \
-jq -r '.data.repository.releases.nodes[0].releaseAssets.nodes | map(select(.name | endswith("Linux-64bit.tar.gz")))[0].downloadUrl' | \
+./jq -r '.data.repository.releases.nodes[0].releaseAssets.nodes | map(select(.name | endswith("Linux-64bit.tar.gz")))[0].downloadUrl' | \
 tr -d '\r' | \
-xargs -I {} curl -kL {} | \
+xargs -I {} curl -skL {} | \
 tar -xz -C .. hugo
