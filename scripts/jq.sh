d7c7b93490827f8161263a3fc197c58ca80e8cfd
XhstormR <10527522+XhstormR@users.noreply.github.com>
2018-03-18 23:54:22 +0800

    Signed-off-by: XhstormR <10527522+XhstormR@users.noreply.github.com>

--- scripts/jq.sh
+++ scripts/jq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git clone --depth 1 --recurse-submodules -j2 https://github.com/stedolan/jq.git && \
-cd jq && \
+git clone --depth 1 --recurse-submodules -j2 https://github.com/stedolan/jq.git jq_git && \
+cd jq_git && \
 mkdir build ; cd build && \
 autoreconf -fi .. && \
 ../configure --with-oniguruma=builtin --enable-shared=no --enable-static=yes CFLAGS='-s -static -Os' && \
