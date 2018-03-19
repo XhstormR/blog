1a09a6f9f32334bcd713ed0e674b781ee6c171c5
XhstormR <10527522+XhstormR@users.noreply.github.com>
2018-03-19 21:42:34 +0800

    Signed-off-by: XhstormR <10527522+XhstormR@users.noreply.github.com>

--- scripts/hugo.sh
+++ scripts/hugo.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-./jq --rawfile abc graphql.txt '.query=$abc' template.json | \
+if [ -f ../bin/hugo ] ; then
+    echo "Cached!"
+    exit 0
+else
+    echo "Not cached!"
+fi
+
+../bin/jq --rawfile abc graphql.txt '.query=$abc' template.json | \
 curl -sk -H "Authorization: bearer $GITHUB_TOKEN" -d @- https://api.github.com/graphql | \
-./jq -r '.data.repository.releases.nodes[0].releaseAssets.nodes | map(select(.name | endswith("Linux-64bit.tar.gz")))[0].downloadUrl' | \
+../bin/jq -r '.data.repository.releases.nodes[0].releaseAssets.nodes | map(select(.name | endswith("Linux-64bit.tar.gz")))[0].downloadUrl' | \
 tr -d '\r' | \
 xargs -I {} curl -skL {} | \
-tar -xz -C .. hugo
+tar -xz -C ../bin hugo
--- scripts/jq.sh
+++ scripts/jq.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+if [ -f ../bin/jq ] ; then
+    echo "Cached!"
+    exit 0
+else
+    echo "Not cached!"
+fi
+
 git clone --depth 1 --recurse-submodules -j2 https://github.com/stedolan/jq.git jq_git && \
 cd jq_git && \
 mkdir build ; cd build && \
 autoreconf -fi .. && \
 ../configure --with-oniguruma=builtin --enable-shared=no --enable-static=yes CFLAGS='-s -static -Os' && \
 make -j4 && \
-cp jq ../.. && \
+cp jq ../../../bin && \
 cd ../..
